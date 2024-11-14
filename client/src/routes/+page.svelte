54e9dc19f8ed84209564b760ad40797960712e1e
saefstroem <alexandersafstrom@proton.me>
2024-11-14 12:11:40 +0100

    #6 Setup svelte project
    - Setup svelte project
    - Setup eslint
    - Setup typescript

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<!--
+This is the index page. It is accessible at the root of the application.
+-->
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
