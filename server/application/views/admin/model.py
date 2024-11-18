cdab00ff648e19e19bcdd7be8fe0fc335056cda0
Konstantinos Rokanas <rokanas@student.chalmers.se>
2024-11-19 00:11:40 +0100

    #10 Add endpoint for getting active model
    This commit adds an admin view containing the first endpoint for getting the currently active model.

---
+++ server/application/views/admin/model.py
@@ -0,0 +1,19 @@
+from django.http import JsonResponse
+from django.views import View
+
+from ...models import Model
+
+class ModelView(View):
+    def get(self, request, check_admin=True):
+        try:
+            active_model = Model.objects.get(status="active")
+            return JsonResponse({
+                "model_id": active_model.model_id,
+                "created_at": active_model.created_at,
+                "version": active_model.version,
+                "weights": active_model.weights,
+                # "status": active_model.status
+            })
+        
+        except Model.DoesNotExist:
+            return JsonResponse({"err": "No active model found"}, status=404)
