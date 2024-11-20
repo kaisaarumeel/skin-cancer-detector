6c3fa4d974562bec09faeece7fc6861e5fca82f5
rokanas <rokanas@student.chalmers.se>
2024-11-20 19:18:02 +0100

    #10 Change weights to base64
    This commit changes the weights format to base64 which is JSON serializable (so that they can be returned in jsonobject without error

--- server/application/views/models/active_model.py
+++ server/application/views/models/active_model.py
@@ -1,3 +1,4 @@
+import base64
 from django.http import JsonResponse
 from django.views import View
 
@@ -14,7 +15,7 @@ class GetActiveModel(View):
                 "model_id": active_model.model_id,
                 "created_at": active_model.created_at,
                 "version": active_model.version,
-                "weights": active_model.weights,
+                "weights": base64.b64encode(active_model.weights).decode('utf-8'),  # convert to base64 string, which is JSON-serializable,
                 # "status": active_model.status #technically redundant
             })
         
