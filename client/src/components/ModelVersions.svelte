ed75a2928a44f13222d34140cec12d4afa4e906c
Konstantinos Rokanas <rokanas@student.chalmers.se>
2024-11-26 23:53:54 +0100

    #31 Connect endpoints with frontend

--- client/src/components/ModelVersions.svelte
+++ client/src/components/ModelVersions.svelte
@@ -1,29 +1,54 @@
 <script lang="ts">
-    type ModelVersion = {
-        name: string;
-        dateAdded: string;
+    import { onMount } from "svelte";
+    import type { AxiosResponse } from "axios";
+    import { API } from "../api";
+
+    type Model = {
+        version: string;
+        created_at: number;
+        hyperparameters: string;
     };
 
-    let modelVersions: ModelVersion[] = [
-        { name: "v1.0", dateAdded: "2024-01-15" },
-        { name: "v1.1", dateAdded: "2024-02-10" },
-        { name: "v2.0", dateAdded: "2024-03-05" },
-        { name: "v3.0", dateAdded: "2024-05-20" },
-        { name: "v1.0", dateAdded: "2024-01-15" },
-        { name: "v1.1", dateAdded: "2024-02-10" },
-        { name: "v2.0", dateAdded: "2024-03-05" },
-        { name: "v3.0", dateAdded: "2024-05-20" },
-        { name: "v1.0", dateAdded: "2024-01-15" },
-        { name: "v1.1", dateAdded: "2024-02-10" },
-        { name: "v2.0", dateAdded: "2024-03-05" },
-        { name: "v3.0", dateAdded: "2024-05-20" },
-    ];
+    let models: Model[] = [];
 
-    let currentModelVersion: ModelVersion = modelVersions[2];
+    let activeModel: Model | null = null;
 
-    function setCurrentVersion(version: ModelVersion): void {
-        currentModelVersion = version;
+    async function getAllModels(): Promise<void> {
+        try {
+            const response: AxiosResponse<{models: any[]}> = await API.get("api/models/all-models/");
+            models = response.data.models;
+
+        } catch (error) {
+            console.error("Getting all models failed:", error);
+        }
     }
+
+    async function getActivemodel(): Promise<void> {
+        try {
+            const response: AxiosResponse<Model> = await API.get("api/models/active-model/");
+            activeModel = response.data;
+
+        } catch (error) {
+            console.error("Getting active model failed:", error);
+        }
+    }
+
+    async function setActiveModel(model: Model): Promise<void> {
+        try {
+            const response: AxiosResponse<{ message: string }> = await API.post(`api/models/swap-model/${model.version}/`);
+            console.log(response.data.message);
+            activeModel = model;
+        } catch (error) {
+            console.error("Setting active model failed:", error);
+        }
+        activeModel = model;
+
+    }
+
+    onMount(() => {
+        getAllModels();
+        getActivemodel();
+    });
 </script>
 
 <div class="bg-white w-full rounded-lg shadow-md p-4 flex flex-col items-start">
@@ -33,7 +58,11 @@
     <div class="mt-4 w-full">
         <p class="text-sm font-medium text-tertiary">Current Model Version:</p>
         <div class="py-2 px-3 bg-green-100 text-green-800 rounded mt-1 text-md">
-            {currentModelVersion.name}
+            {#if activeModel}
+                v{activeModel.version}.0
+            {:else}
+                <em>No active model</em>
+            {/if}
         </div>
     </div>
 
@@ -41,22 +70,22 @@
         Switch the current Model Version:
     </p>
     <ul class="mt-1 w-full space-y-2 text-gray-700 max-h-80 overflow-y-auto">
-        {#each modelVersions as version (version)}
+        {#each models as model (model.version)}
             <li>
                 <button
                     type="button"
                     class="w-full flex items-center justify-between py-2 px-3 bg-gray-100 rounded hover:bg-gray-200 focus:outline-none"
-                    on:click={() => setCurrentVersion(version)}
+                    on:click={() => setActiveModel(model)}
                     on:keydown={(e) =>
-                        e.key === "Enter" && setCurrentVersion(version)}
+                        e.key === "Enter" && setActiveModel(model)}
                 >
                     <div>
-                        <span class="text-tertiary">{version.name}</span>
+                        <span class="text-tertiary">v{model.version}.0</span>
                         <span class="text-tertiary text-xs ml-2"
-                            >{version.dateAdded}</span
+                            >{new Date(model.created_at * 1000).toLocaleDateString()}</span
                         >
                     </div>
-                    {#if version === currentModelVersion}
+                    {#if model === activeModel}
                         <span class="text-green-600 text-xs font-regular"
                             >(Current)</span
                         >
