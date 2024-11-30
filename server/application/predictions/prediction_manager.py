84e91625ea1f50b5d3209a73114b95d5c6abdc0e
Erik <elindstr@student.chalmers.se>
2024-11-30 21:31:32 +0100

    #23 Add Prediction Data Preprocessing (WIP)
    This commit adds code that extracts and preprocesses the user data submitted for inference. To be continued...
    
    Co-authored-by: Kaisa Arumeel <kaisa.arumeel@gmail.com>

--- server/application/predictions/prediction_manager.py
+++ server/application/predictions/prediction_manager.py
@@ -1,9 +1,12 @@
 import warnings
 import time
 import threading
+import base64
+import pickle
 from pathlib import Path
 
 import tensorflow as tf
+import numpy as np
 
 # Custom modules
 from application.views.jobs.state import PREDICTION_JOBS
@@ -27,7 +30,10 @@ def manage_predictions():
     warnings.simplefilter("always", UserWarning)
 
     # Load the active model from the database
-    model = load_active_model_from_db(abs_db_path)
+    model, hyperparameters = load_active_model_from_db(abs_db_path)
+
+    # Load the feature scaler
+    tabular_scaler = getScaler(hyperparameters)
 
     # Predictions loop
     while True:
@@ -37,7 +43,9 @@ def manage_predictions():
         # Check if the model should be reloaded
         if model_reload_event.is_set() or model == None:
             print("Reloading model...")
-            model = load_active_model_from_db(abs_db_path)
+            model, hyperparameters = load_active_model_from_db(abs_db_path)
+            # Reload the feature scaler
+            tabular_scaler = getScaler(hyperparameters)
 
             # Clear the event for reuse
             model_reload_event.clear()
@@ -57,16 +65,22 @@ def manage_predictions():
             for _ in range(min(BATCH_SIZE, PREDICTION_JOBS.qsize()))
         ]
 
+        # If there are no Jobs to be processed, skip this loop iteration
+        if not jobs_batch:
+            continue
+
         # Prepare a list of resized images that fit the model input size
         resized_images = preprocess_images(
             extract_images(jobs_batch), get_model_input_shape(model)
         )
 
-        # TODO Prepare input batch function call
+        # Extract the tabular features
+        tabular_features = extract_tabular_features(jobs_batch, tabular_scaler)
 
         # TODO call predict() on batch
+        ### NOTE: if prediction doesn't work, uncomment weights statement inside ml/persistence.py
 
-        # TODO return results
+        # TODO map results to request_ids and write them to database
 
         print("Processing predictions...")
 
@@ -76,7 +90,7 @@ def manage_predictions():
 
 def start_prediction_manager():
     # Create separate thread that will manage predictions
-    # daemon=True ensure thread is terminated alongside main thread
+    # daemon=True ensures thread is terminated alongside main thread
     prediction_thread = threading.Thread(daemon=True, target=manage_predictions)
 
     # Initialise thread
@@ -88,16 +102,6 @@ def signal_model_reload():
     model_reload_event.set()
 
 
-def extract_images(jobs):
-    images = []
-    for job in jobs:
-        parameters = job.parameters
-        image = parameters.get("image")
-        images.append(image)
-
-    return images
-
-
 def get_model_input_shape(model):
     # Extract the input shape from the model
     target_input_shape = model.input_shape[0]
@@ -107,3 +111,56 @@ def get_model_input_shape(model):
 
     # Return target height and width
     return (target_height, target_width)
+
+
+def getScaler(hyperparameters):
+    # Decode and unpickle the scaler
+    encoded_scaler = hyperparameters["tabular_scaler"]
+    pickled_scaler = base64.b64decode(encoded_scaler)
+
+    # Return Scaler object
+    return pickle.loads(pickled_scaler)
+
+
+def extract_images(jobs):
+    images = []
+    for job in jobs:
+        parameters = job.parameters
+        image = parameters.get("image")
+        images.append(image)
+
+    # Stack images in 2D array
+    images = np.column_stack((images))
+
+    return images
+
+
+def extract_tabular_features(jobs, scaler):
+    # Temp lists to hold the features before stacking in 2D array
+    ages = []
+    localizations = []
+    sexes = []
+
+    for job in jobs:
+        # Extract feature fields
+        age = job.parameters.get("age")
+        localization = job.parameters.get("localization")
+        sex = job.parameters.get("sex")
+
+        # TODO one-hot encode localization
+
+        # Convert sex to a boolean
+        is_male = sex.lower() == "male"
+
+        # Append to temp lists
+        ages.append(age)
+        localizations.append(localization)
+        sexes.append(is_male)
+
+    # Stack features in 2D array
+    tabular_features = np.column_stack((ages, localizations, sexes))
+
+    # Standardize the distribution using the scaler
+    tabular_features = scaler.transform(tabular_features)
+
+    return tabular_features
