2852f5b19792c69baab6d47e02ff05880a6af455
arumeel <kaisa.arumeel@gmail.com>
2024-12-05 13:44:15 +0100

    #52 Make login and signup work with "enter"

--- client/src/components/LoginForm.svelte
+++ client/src/components/LoginForm.svelte
@@ -26,11 +26,11 @@
 
       // Check if the user is an admin and redirect accordingly
       const adminResponse = await API.get('api/is_admin/');
-            if (adminResponse.data.is_admin) {
-                goto('/admin'); // If the user is an admin, redirect to the admin page
-            } else {
-              goto('/upload');
-            }
+      if (adminResponse.data.is_admin) {
+          goto('/admin'); // If the user is an admin, redirect to the admin page
+        } else {
+          goto('/upload');
+        }
     } catch (err) {
       console.error('Error occurred during signup:', err);
 
@@ -45,6 +45,13 @@
       }
     }
   }
+
+  // Function to handle form submission when pressing Enter
+  function handleKeydown(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      Signin();
+    }
+  }
 </script>
 
 <div class="flex flex-col max-w-md w-full text-center justify-center">
@@ -52,13 +59,30 @@
   <h2 class="text-3xl font-light text-secondary mb-6 hidden lg:block"> Log in </h2>
   <p class="text-tertiary mb-5">Welcome back, you’ve been missed!</p>
   <div class="mb-4">
-    <input type="username" bind:value={username} placeholder="Username" class="w-full p-2 border border-gray-300 rounded-md outline-none mt-1 focus:border-secondary">
+    <input
+      type="username"
+      bind:value={username}
+      placeholder="Username"
+      class="w-full p-2 border border-gray-300 rounded-md outline-none mt-1 focus:border-secondary"
+      on:keydown={handleKeydown} 
+    />
   </div>
   <div class="mb-4">
-    <input type="password" bind:value={password} placeholder="Password" class="w-full p-2 border border-gray-300 rounded-md outline-none mt-1 focus:border-secondary">
+    <input
+      type="password"
+      bind:value={password}
+      placeholder="Password"
+      class="w-full p-2 border border-gray-300 rounded-md outline-none mt-1 focus:border-secondary"
+      on:keydown={handleKeydown} 
+    />
   </div>
   {#if errorMessage}
-  <p class="text-red-500 text-xs font-light">{errorMessage}</p>
+    <p class="text-red-500 text-xs font-light">{errorMessage}</p>
   {/if}
-  <button on:click={Signin} class="w-full p-3 bg-primary text-white font-light rounded-md cursor-pointer mt-4 transition-colors hover:bg-secondary shadow-l">Sign in</button>
+  <button
+    on:click={Signin}
+    class="w-full p-3 bg-primary text-white font-light rounded-md cursor-pointer mt-4 transition-colors hover:bg-secondary shadow-l"
+  >
+    Sign in
+  </button>
 </div>
