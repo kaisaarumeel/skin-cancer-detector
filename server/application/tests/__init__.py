720a5136f0beaaa5473062e47c78783be014b80a
Arvin Rahimi <arvinra@student.chalmers.se>
2024-12-06 14:10:12 +0100

    47 fixed typo in init file

--- server/application/tests/__init__.py
+++ server/application/tests/__init__.py
@@ -5,7 +5,7 @@ from .get_all_requests import GetAllRequestsTests
 from .get_all_users import GetAllUsersTests
 from .create_request import CreateRequestTests
 from .ml import MLTests
-from .retrain import RetrainTestsHEAD
+from .retrain import RetrainTests
 from .get_specific_request import GetSpecificRequestTests
 from .get_requests_by_username import GetRequestsByUsernameTests
 from .predictions import PreprocessDataTests
