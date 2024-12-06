26c2641a90c39bcf6ba11a1cad32d5f9ad71656b
Konstantinos Rokanas <rokanas@student.chalmers.se>
2024-12-06 17:33:16 +0100

    #54 Fix csrf endpoint

--- server/application/views/get_csrf_token.py
+++ server/application/views/get_csrf_token.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
+from django.views import View
 from django.views.decorators.csrf import ensure_csrf_cookie
 
 @ensure_csrf_cookie
-def GetCSRFToken(request):
-    return JsonResponse({'message': 'CSRF cookie set'})
+class GetCSRFToken(View):
+    def get(self, request):
+        return JsonResponse({'message': 'CSRF cookie set'})
