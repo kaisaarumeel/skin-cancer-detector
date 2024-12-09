830c7f11ff7b21a5a3e41f3e5bc95baa67e77812
Amirpooya <amirpooya78@gmail.com>
2024-12-09 17:00:53 +0100

    #63 imports the test file to the __init__.py

--- server/application/tests/__init__.py
+++ server/application/tests/__init__.py
@@ -10,3 +10,4 @@ from .get_specific_request import GetSpecificRequestTests
 from .get_requests_by_username import GetRequestsByUsernameTests
 from .predictions import PreprocessDataTests
 from .delete_user import DeleteUserTests
+from .get_total_datapoints import GetTotalDataPointsTests
