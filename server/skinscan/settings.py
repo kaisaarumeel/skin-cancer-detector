ec0e8589a7e4e9d1cb20d8a6330d7673260156c6
Erik <elindstr@student.chalmers.se>
2024-12-09 18:45:08 +0100

    #54 Try Disabling CORS Restrictions

--- server/skinscan/settings.py
+++ server/skinscan/settings.py
@@ -146,10 +146,11 @@ if not DEBUG:
 
 # CORS settings (for prod.)
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    #"http://localhost:3000",
+    #"http://127.0.0.1:3000",
+    #"http://localhost:5173",
+    #"http://127.0.0.1:5173",
+    "*", # Allow everything for testing purposes
 ]
 CORS_ALLOW_CREDENTIALS = True
 
