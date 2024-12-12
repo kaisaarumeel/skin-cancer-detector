5367ed2658cb97c0a8b65b8ad1c415697cdb93bc
Erik <elindstr@student.chalmers.se>
2024-12-12 20:45:54 +0100

    #54 Hardcode Backend IP In Axios API BaseURL

--- client/src/api.ts
+++ client/src/api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // Use injected environment variable if it exists otherwise development default
 // Stupid hacky fix but the alternatives are unnecessarily complex
-let baseURL = import.meta.env.VITE_BACKEND_SKINSCAN || "http://localhost:666";
+//let baseURL = import.meta.env.VITE_BACKEND_SKINSCAN || "http://localhost:666";
+let baseURL = "http://164.92.176.222:8000"
 
 export const API = axios.create({
     baseURL: baseURL,
