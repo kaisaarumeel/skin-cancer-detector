a6043897a41334e0478b01e85da215f5760655e5
Konstantinos Rokanas <rokanas@student.chalmers.se>
2024-12-12 15:51:30 +0100

    #54 Attempt fix vite config

--- client/src/api.ts
+++ client/src/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Use injected environment variable if it exists otherwise development default
 // Stupid hacky fix but the alternatives are unnecessarily complex
-let baseURL = import.meta.env.VITE_BACKEND_SKINSCAN || "http://localhost:8000";
+let baseURL = import.meta.env.VITE_BACKEND_SKINSCAN || "http://localhost:666";
 
 export const API = axios.create({
     baseURL: baseURL,
