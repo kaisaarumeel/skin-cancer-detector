a37243d035283684bb5606a66099da3538f2d9ee
Erik <elindstr@student.chalmers.se>
2024-12-12 14:00:31 +0100

    #54 Fix Base URL Bug In Local Development Mode

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,7 +1,17 @@
 import axios from "axios";
 
-// Use injected environment variable if it exists otherwise dev default
+// Use injected environment variable if it exists otherwise development default
+// Stupid hacky fix but the alternatives are unnecessarily complex
+let baseURL = "http://localhost:8000";
+// Check if the Node process exists (it does during build stage)
+if (typeof process !== "undefined") {
+    // TypeScript demands this check :)))
+    if (process.env.CLIENT_API_BASE_URL) {
+        baseURL = process.env.CLIENT_API_BASE_URL;
+    }
+}
+
 export const API = axios.create({
-    baseURL: process.env.CLIENT_API_BASE_URL || "http://localhost:8000",
+    baseURL: baseURL,
     withCredentials: true,
 });
