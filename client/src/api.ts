80311f16c0fd1c94b4f056ef60533b526e35f0f1
Konstantinos Rokanas <rokanas@student.chalmers.se>
2024-12-12 14:43:07 +0100

    #54 Fix env

--- client/src/api.ts
+++ client/src/api.ts
@@ -2,14 +2,7 @@ import axios from "axios";
 
 // Use injected environment variable if it exists otherwise development default
 // Stupid hacky fix but the alternatives are unnecessarily complex
-let baseURL = "http://localhost:8000";
-// Check if the Node process exists (it does during build stage)
-if (typeof process !== "undefined") {
-    // TypeScript demands this check :)))
-    if (process.env.CLIENT_API_BASE_URL) {
-        baseURL = process.env.CLIENT_API_BASE_URL;
-    }
-}
+let baseURL = import.meta.env.VITE_BACKEND_SKINSCAN || "http://localhost:8000";
 
 export const API = axios.create({
     baseURL: baseURL,
