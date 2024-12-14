685e839eb4c6b937472fc8d29238bb646fbadc36
Konstantinos Rokanas <rokanas@student.chalmers.se>
2024-12-14 17:51:42 +0100

    #54 Fix request urls

--- client/src/api.ts
+++ client/src/api.ts
@@ -2,8 +2,9 @@ import axios from "axios";
 
 // Use injected environment variable if it exists otherwise development default
 // Stupid hacky fix but the alternatives are unnecessarily complex
+
 //let baseURL = import.meta.env.VITE_BACKEND_SKINSCAN || "http://localhost:666";
-let baseURL = "http://164.92.176.222:8000"
+let baseURL = "http://164.92.176.222"
 
 export const API = axios.create({
     baseURL: baseURL,
