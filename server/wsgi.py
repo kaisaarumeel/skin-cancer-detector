ae38b1e3a0498bc99c8d8544be7feae96b8b0c58
Konstantinos Rokanas <rokanas@student.chalmers.se>
2024-12-14 18:12:38 +0100

    #Fix wsgi conf and attempt fix nginx config

--- server/wsgi.py
+++ server/wsgi.py
@@ -8,13 +8,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import django
 from django.core.wsgi import get_wsgi_application
-
-#sys.path.append("..")
-#from setup.setup import setup
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "skinscan.settings")
 
-#setup()
+from setup.setup import setup
+
+setup()
 
 application = get_wsgi_application()
