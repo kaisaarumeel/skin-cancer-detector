f39afa71dafc1bce996eabed07d67d2aeefd72e0
Konstantinos Rokanas <rokanas@student.chalmers.se>
2024-12-14 19:29:01 +0100

    #54 Test env variables

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,10 +1,6 @@
 import axios from "axios";
 
-// Use injected environment variable if it exists otherwise development default
-// Stupid hacky fix but the alternatives are unnecessarily complex
-
-//let baseURL = import.meta.env.VITE_BACKEND_SKINSCAN || "http://localhost:666";
-let baseURL = "http://164.92.176.222"
+let baseURL = import.meta.env.VITE_BACKEND_SKINSCAN || "http://localhost:8000";
 
 export const API = axios.create({
     baseURL: baseURL,
