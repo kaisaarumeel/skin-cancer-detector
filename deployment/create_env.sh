a3cc868789d0d1124161e84d234aefeffa135d06
Konstantinos Rokanas <rokanas@student.chalmers.se>
2024-12-14 20:50:34 +0100

    #54 Fix client env import again

--- deployment/create_env.sh
+++ deployment/create_env.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # ensure env arguments are provided
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
   echo "❌ Error: Missing required arguments."
-  echo "Usage: $0 <SECRET_KEY> <SETUP_PROMPT_1> <SETUP_PROMPT_2>"
+  echo "Usage: $0 <VITE_BACKEND_SKINSCAN> <SECRET_KEY> <SETUP_PROMPT_1> <SETUP_PROMPT_2>"
   exit 1
 fi
 
 # assign args to env variables
-SECRET_KEY="$1"
-SETUP_PROMPT_1="$2"
-SETUP_PROMPT_2="$3"
+VITE_BACKEND_SKINSCAN="$1"
+SECRET_KEY="$2"
+SETUP_PROMPT_1="$3"
+SETUP_PROMPT_2="$4"
 
 # define target env file
 ENV_FILE="/services/skinscan/.env"
@@ -18,6 +19,7 @@ ENV_FILE="/services/skinscan/.env"
 # create or overwrite env file
 echo "Writing to $ENV_FILE..."
 sudo bash -c "cat > $ENV_FILE" <<EOF
+VITE_BACKEND_SKINSCAN=$VITE_BACKEND_SKINSCAN
 SECRET_KEY=$(echo $SECRET_KEY | base64 -d)
 SETUP_PROMPT_1=$SETUP_PROMPT_1
 SETUP_PROMPT_2=$SETUP_PROMPT_2
