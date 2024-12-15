1b90d7058218e7528847049b043c9a1e23ffa38e
Erik <elindstr@student.chalmers.se>
2024-12-15 23:09:16 +0100

    #75 Fix Black Formatting

--- dev_utils/delete-requests-in-db.py
+++ dev_utils/delete-requests-in-db.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 # Path to SQLite database file
-db_path = '../db_app.sqlite3'
+db_path = "../db_app.sqlite3"
 
 # Connect to the SQLite database
 conn = sqlite3.connect(db_path)
@@ -9,19 +9,19 @@ cursor = conn.cursor()
 
 try:
     # Step 1: Check if there are any records in the Requests table
-    cursor.execute('SELECT COUNT(*) FROM requests;')
+    cursor.execute("SELECT COUNT(*) FROM requests;")
     record_count = cursor.fetchone()[0]
 
     if record_count > 0:
         print(f"There are {record_count} records in the 'requests' table.")
-        
+
         # Step 2: Delete all records from the Requests table
-        cursor.execute('DELETE FROM requests;')
+        cursor.execute("DELETE FROM requests;")
         conn.commit()
         print("All records have been deleted from the 'requests' table.")
     else:
         print("No records found in the 'requests' table.")
-    
+
 except sqlite3.Error as e:
     print(f"An error occurred: {e}")
 finally:
