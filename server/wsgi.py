1999e8724326f8ce6da41ef206da3e79f37a26a8
Erik <elindstr@student.chalmers.se>
2024-12-16 01:55:46 +0100

    #54 Fix WSGI Setup And WSGI Application Path

--- server/wsgi.py
+++ server/wsgi.py
@@ -8,13 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "skinscan.settings")
 
-from setup.setup import setup
-
-setup()
-
 application = get_wsgi_application()
+
+from setup.setup import setup
+setup()
