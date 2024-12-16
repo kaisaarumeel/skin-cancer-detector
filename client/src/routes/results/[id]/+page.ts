38fc6ca6ac0e203f523672cc29927da3d71922a9
Erik <elindstr@student.chalmers.se>
2024-12-16 04:26:00 +0100

    #54 Try To Exclude Dynamic Page From Prerendering

--- client/src/routes/results/[id]/+page.ts
+++ client/src/routes/results/[id]/+page.ts
@@ -1,5 +1,8 @@
 import type { PageLoad } from './$types';
 
+export const prerender = false;
+export const ssr = false;
+
 export const load: PageLoad = ({ params }) => {
   return {
     id: params.id
