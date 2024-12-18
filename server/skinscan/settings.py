00e49421c842183d1e48d3ae8b8ce64bde809939
Erik <elindstr@student.chalmers.se>
2024-12-18 18:11:04 +0100

    #54 Disable Django HTTPS Settings When Running DEBUG=False

--- server/skinscan/settings.py
+++ server/skinscan/settings.py
@@ -145,9 +145,10 @@ SESSION_COOKIE_SECURE = False
 
 # Security settings (for production)
 if not DEBUG:
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
+    # Enable these if we setup a domain name + HTTPS
+    # SECURE_SSL_REDIRECT = True
+    # SESSION_COOKIE_SECURE = True
+    # CSRF_COOKIE_SECURE = True
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
 
